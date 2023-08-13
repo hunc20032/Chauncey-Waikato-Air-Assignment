2cf5e732fd1a266db2268f789d97c8ff7a19b89d
hunc20032 <99440045+hunc20032@users.noreply.github.com>
2023-08-13 22:12:40 +0000

    added ask number of people

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("cool")
-nums = int(input("so this works now "))
-print(nums)
+# welcome user
+print("Hello user,")
+name = input("What is your name? ")
+print("Hello",name,"I hope you are having a good day.")
+num_person = int(input("How many humans do you have in your possession?"))
+# user data
+user_data = {
+	"name": name,
+	"people": num_person,
+	
+}
+print(user_data)
