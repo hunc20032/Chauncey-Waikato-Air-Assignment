825bcd93093307dca9ac9605e3c75980522fc1af
hunc20032 <99440045+hunc20032@users.noreply.github.com>
2023-11-16 02:02:38 +0000

    fixed full seat problem and made it print 1 ticket number per-person

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # import libraries
 import random
 import sys
-# to do: fix number of full seats (with dictionaries)
 
-# check if there are numbers in a values
+
+# classmethodheck if there are numbers in a values
 def contains_number(string):
 	return any(char.isdigit() for char in string)
 	
@@ -25,28 +25,43 @@ def get_destination_price():
 		except ValueError:
 			print("Invalid input")
 
-# ask user for the number of people they are traveling with (fix me) "don't worry cup i'm on it"
-def get_num_people():
+# ask how many people they are booking for and tell them how many seats are left
+# return the number of people to be used later and change the total number of people on the flight
+def get_num_people(destination_price):
 	while True:
 		try:
 			if destination_price[0] == 54.99:
-				num_full_seat = full_bookings["Auckland"]
-				print(f"We have {248 - num_full_seat} seats left on the Auckland flight ")
+				print("We have",248 - full_bookings["Auckland"],"seats left on the Auckland flight ")
+				num_people = int(input("How many people are you booking for? "))
+				if 0 < num_people and num_people < (249 - full_bookings["Auckland"]):
+					full_bookings["Auckland"] += num_people
+					return full_bookings["Auckland"], num_people
+				else:
+					print("Exceeds the number of seats available")
+					
 			elif destination_price[0] == 39.99:
-				num_full_seat = full_bookings["Hamilton"]
-				print(f"We have {248 - num_full_seat} seats left on the Hamilton flight ")
+				print("We have",248 - full_bookings["Hamilton"],"seats left on the Hamilton flight ")
+				num_people = int(input("How many people are you booking for? "))
+				if 0 < num_people and num_people < (249 - full_bookings["Hamilton"]):
+					full_bookings["Hamilton"] += num_people
+					return full_bookings["Hamilton"], num_people
+				else:
+					print("Exceeds the number of seats available")
+					
 			elif destination_price[0] == 59.99:
-				num_full_seat = full_bookings["Rotorua"]
-				print(f"We have {248 - num_full_seat} seats left on the Rotorua flight ")
-			num_people = int(input("How many people are you booking for? "))
-			if 0 < num_people < (249 - num_full_seat):
-				return num_full_seat + num_people, num_people
-			else:
-				print("Exceeds the number of seats available")
+				print("We have",248 - full_bookings["Rotorua"],"seats left on the Rotorua flight ")
+				num_people = int(input("How many people are you booking for? "))
+				if 0 < num_people and num_people < (249 - full_bookings["Rotorua"]):
+					full_bookings["Rotorua"] += num_people
+					return full_bookings["Rotorua"], num_people
+				else:
+					print("Exceeds the number of seats available")
+
 		except ValueError:
 			print("Invalid input")
 
-# ask user what class of seat they want
+# ask user what class of seat they want and tell them the price of the seat
+# return the seat class and price to be used later
 def get_seat_class():
 	while True:
 		try:
@@ -64,11 +79,12 @@ def get_seat_class():
 			print("Invalid input")
 
 # ask user when they want to depart
+# return the day and price to be used later
 def get_leave_day():
 	while True:
 		try:
 			leave_day = int(input("""When are you planning on departing?
-Type... 1: tomorrow, 2: the day after, or 3: a later date. """))
+1 for tomorrow, 2 for the day after, or 3 for a later date. """))
 			if leave_day == 1:
 				return 1.8, "tomorrow"
 			elif leave_day == 2:
@@ -80,7 +96,7 @@ Type... 1: tomorrow, 2: the day after, or 3: a later date. """))
 		except ValueError:
 			print("Invalid input")
 
-# ask user there name
+# ask user there name and check if it is suitable
 def get_username():
 	while True:
 		try:
@@ -99,7 +115,7 @@ def get_username():
 	print(f"Hello {name}, I hope you are having a good day. ")
 	return name
 
-# calculate the price
+# calculate the total price
 def calculate_full_price(destination_price, num_people_ask, fclass, leave_day):
 	full_price = (destination_price[0] * num_people_ask[1]) * fclass[0]
 	if num_people_ask[0] > 124:
@@ -117,20 +133,23 @@ def print_ticket_info(name, num_people_ask, destination_price, fclass, leave_day
 	Destination: {destination_price[1]}
 	Seat Class: {fclass[1]}
 	Flight Date: {leave_day[1]}
-	Price: ${full_price}
-	Ticket number: {random.randint(1, 248)}
-	""")
+	Price: ${full_price}""")
+	num_tickets = num_people_ask[0]
+	for i in range(num_people_ask[1]):
+		num_tickets += 1
+		print(f"	Ticket number: {num_tickets}")
 
-# Ask the user if they want to book another ticket
+# Ask the user if they want to book another ticket and repeat the questions
 def ask_second_booking():
 	while True:
 		try:
 			booking_confirmation = input("Would you like to book another ticket? (Y/N)" )
 			booking_confirmation = booking_confirmation.upper().strip()
 			if booking_confirmation == "Y":
+				print("Hello and welcome to Waikato airborn,")
 				name = get_username()
 				destination_price = get_destination_price()
-				num_people_ask = get_num_people()
+				num_people_ask = get_num_people(destination_price)
 				fclass = get_seat_class()
 				leave_day = get_leave_day()
 				full_price = calculate_full_price(destination_price, num_people_ask, fclass, leave_day)
@@ -147,16 +166,17 @@ def ask_second_booking():
 def middle_man():
 	ask_second_booking()
 
-# Generate random bookings
+# Generate random bookings (fix me)
 full_bookings = {
-	"Auckland": random.randint(0, 248),
-	"Hamilton": random.randint(0, 248),
-	"Rotorua": random.randint(0, 248)}
+	"Auckland": random.randint(0, 247),
+	"Hamilton": random.randint(0, 247),
+	"Rotorua": random.randint(0, 247)}
 
+# run all defs
 print("Hello and welcome to Waikato airborn,")
 name = get_username()
 destination_price = get_destination_price()
-num_people_ask = get_num_people()
+num_people_ask = get_num_people(destination_price)
 fclass = get_seat_class()
 leave_day = get_leave_day()
 full_price = calculate_full_price(destination_price, num_people_ask, fclass, leave_day)
